6acb77a2a673891e5a2a18819486dd75bcf53bca
sbmaxx <sbmaxx@yandex-team.ru>
2018-05-12 13:13:40 +0300

    checkout sync way

--- cli/checkout.js
+++ cli/checkout.js
@@ -1,51 +1,34 @@
 const util = require('util');
 const fs = require('fs-extra');
 const path = require('path');
-const spawn = require('child_process').spawn;
+const spawn = require('child_process').spawnSync;
+const chalk = require('chalk');
 
 const [ checkoutDirectory = path.resolve('./checkout'), pullRequest = 'master' ] = process.argv.slice(2);
 const cwd = process.cwd();
 
- function wrapTryCatch(fn) {
-    try {
-        return await fn();
-    } catch(e) {
-        console.error(e);
-        process.exit(1);
-    }
-}
-
-function exec(cmd, opts = { cwd: checkoutDirectory }) {
-    const { cwd } = opts;
-
+function exec(cmd, cwd = checkoutDirectory) {
     console.log(`${cwd} $ ${cmd}`);
-
-    const ps = spawn(cmd, { stdio: 'inherit', shell: true, cwd });
-
-    return new Promise((resolve, reject) => {
-        ps.on('close', code => code === 0 ? resolve() : reject(code));
-    });
+    return spawn(cmd, { stdio: 'inherit', shell: true, cwd });
 }
 
-async function updateRepository() {
-    await exec('git fetch --all');
+function updateRepository() {
+    return exec('git fetch --all');
 }
 
-async function cloneRepository() {
+function cloneRepository() {
     const remotes = `
         [remote "origin"]
         fetch = +refs/heads/*:refs/remotes/origin/*
         fetch = +refs/pull/*/head:refs/remotes/origin/pr/*
     `.trim();
 
-    await wrapTryCatch(async () => {
-        await exec(`git clone https://github.com/turboext/css.git ${checkoutDirectory}`, { cwd });
-        await exec(`echo '${remotes}' >> .git/config`);
-        await exec(`git fetch --all`);
-    });
+    exec(`git clone https://github.com/turboext/css.git ${checkoutDirectory}`, cwd);
+    exec(`echo '${remotes}' >> .git/config`);
+    exec(`git fetch --all`);
 }
 
-async function checkoutPR(pullRequest) {
+function checkoutPR(pullRequest) {
     if (pullRequest === 'master') {
         exec('git checkout master && git pull --rebase');
     } else {
@@ -53,13 +36,7 @@ async function checkoutPR(pullRequest) {
     }
 }
 
-(async () => {
-    try {
-        const exists = await fs.exists(checkoutDirectory);
-        exists ? await updateRepository() : await cloneRepository();
+const exists = fs.existsSync(checkoutDirectory);
+exists ? updateRepository() : cloneRepository();
 
-        await checkoutPR(pullRequest);
-    } catch(e) {
-        console.log(e)
-    }
-})();
+checkoutPR(pullRequest);
